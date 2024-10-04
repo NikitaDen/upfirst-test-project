4df9d3135552a537471a7be7a1a7681d65ae5b23
Nikita Denisov <nickdnsv@gmail.com>
2024-10-04 23:10:21 +0300

    adjust page's MainContent component and minor style adjustments

--- src/pages/home/ui/mainContent/MainContent.tsx
+++ src/pages/home/ui/mainContent/MainContent.tsx
@@ -1,6 +1,92 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
+import { Card } from '@/entities/card/ui'
+import { Post } from '@/entities/card/model'
+import { loadPosts } from '@/pages/home/api'
+import { Pagination } from '@/features/pagination/ui'
 import s from './mainContent.module.scss'
 
+const POSTS_PER_PAGE = 7
+
+// TODO page index state in URL
 export const MainContent = memo(() => {
-  return <main className={s.mainContent}>Main Content</main>
+  const [posts, setPosts] = useState<Post[]>([])
+  const [currentPageIndex, setCurrentPageIndex] = useState(0)
+  const [requestState, setRequestState] = useState<'pending' | 'error' | 'idle'>('idle')
+  const [totalPostsCount, setTotalPostsCount] = useState(0)
+
+  const isUiDisabled = requestState === 'pending'
+
+  useEffect(() => {
+    // Flag that allows to ignore request's returned result in case same request is triggered again
+    let ignore = false
+
+    const fetchRemotePosts = async () => {
+      try {
+        setRequestState('pending')
+
+        const { posts, totalCount } = await loadPosts(0, POSTS_PER_PAGE)
+
+        if (!ignore) {
+          setPosts(posts)
+          setTotalPostsCount(totalCount)
+          setRequestState('idle')
+        }
+      } catch (e) {
+        if (!ignore) {
+          setRequestState('error')
+          console.log('Could not load posts from the server: ', e)
+        }
+      }
+    }
+
+    fetchRemotePosts()
+
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  const handlePageChange = async (index: number) => {
+    try {
+      setCurrentPageIndex(index)
+      setRequestState('pending')
+
+      const { posts, totalCount } = await loadPosts(index, POSTS_PER_PAGE)
+
+      setPosts(posts)
+      setTotalPostsCount(totalCount)
+      setRequestState('idle')
+    } catch (e) {
+      setRequestState('error')
+
+      // Return pagination state to the page index request was sent from
+      setCurrentPageIndex(currentPageIndex)
+
+      console.log('Could not load posts from the server: ', e)
+    }
+  }
+
+  return (
+    <main className={s.mainContent}>
+      <ul className={s.postsList}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Card
+              key={post.id}
+              {...post}
+            />
+          </li>
+        ))}
+      </ul>
+
+      {totalPostsCount > 0 && (
+        <Pagination
+          disabled={isUiDisabled}
+          currentPageIndex={currentPageIndex}
+          totalPages={Math.ceil(totalPostsCount / POSTS_PER_PAGE)}
+          onPageIndexChange={handlePageChange}
+        />
+      )}
+    </main>
+  )
 })
