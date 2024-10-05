09f4a8d2c649d2e3507721328495c11deba0b247
Nikita Denisov <nickdnsv@gmail.com>
2024-10-05 12:00:43 +0300

    adjust Card toggling logic (collapsed state) and UI

--- src/entities/card/ui/Card.tsx
+++ src/entities/card/ui/Card.tsx
@@ -1,30 +1,49 @@
-import { memo, useState } from 'react'
+import React, { memo, useState } from 'react'
 import { Post } from '@/entities/card/model'
 import { Button, Chip } from '@/shared/ui'
 import { formatToLocalDate } from '@/shared/lib'
 import classNames from 'classnames'
+import { handleTextFreeClick } from '@/entities/card/lib/handleTextFreeClick.ts'
 import s from './card.module.scss'
 
 const BUTTONS_COUNT = 24
 
 const createArrayFromNumber = (num: number) => Array.from(new Array(num)).map((_, index) => index)
 
-// Generate content to showcase flexbox usage
+// Generate content to showcase grid usage
 const buttonsArray = createArrayFromNumber(BUTTONS_COUNT)
 
 const defaultTitle = 'Default Title'
 const defaultBody = 'Default Body'
 
+type CardProps = Post & {
+  onDelete?: () => unknown
+}
+
 export const Card = memo(
-  ({ status, createdAt, title = defaultTitle, body = defaultBody }: Post) => {
+  ({ onDelete, status, createdAt, title = defaultTitle, body = defaultBody }: CardProps) => {
     const [isCollapsed, setIsCollapsed] = useState(true)
 
     return (
       <article
-        onClick={() => setIsCollapsed(!isCollapsed)}
+        onClick={(event) => {
+          if (isCollapsed) {
+            setIsCollapsed(!isCollapsed)
+          } else {
+            handleTextFreeClick(
+              event,
+              () => {
+                setIsCollapsed(true)
+              },
+              ['.' + s.collapseIndicator]
+            )
+          }
+        }}
         className={classNames(s.card, { [s.collapsed]: isCollapsed })}
       >
         <header className={s.header}>
+          <small className={s.collapseIndicator}>↑</small>
+
           <Chip
             className={s.statusChip}
             status={status}
@@ -40,7 +59,14 @@ export const Card = memo(
 
         <div className={s.cardContent}>
           {buttonsArray.map((button) => (
-            <Button key={button}>Control {button}</Button>
+            <Button
+              variant={'danger'}
+              className={s.removeButton}
+              key={button}
+              onClick={onDelete}
+            >
+              Remove {button}
+            </Button>
           ))}
         </div>
       </article>
